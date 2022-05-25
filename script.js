626cf23fe0f16e38397d3f520b6d3a61540ade78
KLIM STOROZHENKO <storozhenko.klim@gmail.com>
2022-05-25 12:44:37 +1000

    Add buttons functionality

--- script.js
+++ script.js
@@ -5,13 +5,9 @@ let useRainbow = false;
 let cellColor = "black";
 
 function getRainbowColor() {
-    if (rainbowFlag === rainbowColors.length - 1)
-        rainbowFlag = 0;
-    else
-        rainbowFlag++;
-    return rainbowColors[rainbowFlag]
+  rainbowFlag = (rainbowFlag === rainbowColors.length - 1) ? 0 : rainbowFlag + 1;
+  return rainbowColors[rainbowFlag];
 }
-
 
 
 function colorCell(e) {
@@ -55,4 +51,14 @@ function generateGrid(size) {
 const resetButton = document.querySelector('.reset-button');
 resetButton.addEventListener('click', e => resetColors());
 
-generateGrid(32);
+const sizeRange = document.querySelector('#size-range');
+sizeRange.addEventListener('change', e => {
+    generateGrid(e.target.value);
+})
+
+const rainbowCheckbox = document.querySelector("#rainbow");
+rainbowCheckbox.addEventListener('change', e => {
+    useRainbow = !useRainbow;
+})
+
+generateGrid(8);
